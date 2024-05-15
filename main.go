aa51060169dad432d18441c2e303330be6b42616
victorbrugnolo <brugnolovictor@gmail.com>
2024-05-15 18:59:37 -0300

    add http server recover example

--- main.go
+++ main.go
@@ -1,17 +1,55 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"runtime/debug"
+)
 
-func myPanic() {
-	panic("Something went wrong")
+// func myPanic() {
+// 	panic("Something went wrong")
+// }
+
+// func main() {
+// 	defer func() {
+// 		if r := recover(); r != nil {
+// 			fmt.Println("Recovered in main:", r)
+// 		}
+// 	}()
+
+// 	myPanic()
+// }
+
+func recoverMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		defer func() {
+			if r := recover(); r != nil {
+				fmt.Println("Recovered panic:", r)
+				debug.PrintStack()
+				http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			}
+		}()
+
+		next.ServeHTTP(w, r)
+	})
 }
 
 func main() {
-	defer func() {
-		if r := recover(); r != nil {
-			fmt.Println("Recovered in main:", r)
-		}
-	}()
+	mux := http.NewServeMux()
 
-	myPanic()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello World"))
+	})
+
+	mux.HandleFunc("/panic", func(w http.ResponseWriter, r *http.Request) {
+		panic("panic")
+	})
+
+	log.Println("Listening on", ":3000")
+
+	if err := http.ListenAndServe(":3000", recoverMiddleware(mux)); err != nil {
+		log.Fatalf("Could not listen on %s: %v\n", ":3000", err)
+	}
 }
