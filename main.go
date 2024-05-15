8a4765cda17dd0545701b5a393c1e4301972bf7b
victorbrugnolo <brugnolovictor@gmail.com>
2024-05-15 18:49:16 -0300

    recover panic

--- main.go
+++ main.go
@@ -1,9 +1,17 @@
 package main
 
+import "fmt"
+
 func myPanic() {
 	panic("Something went wrong")
 }
 
 func main() {
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("Recovered in main:", r)
+		}
+	}()
+
 	myPanic()
 }
